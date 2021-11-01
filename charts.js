86320e76a9cebec924ccd8daedc401a94ff722c7
Elle Jacobs <enj657@gmail.com>
2021-11-01 14:15:02 -0500

    modifying index and moving charts file

--- charts.js
+++ charts.js
@@ -72,10 +72,8 @@ function buildCharts(sample) {
     // 7. Create the yticks for the bar chart.
     // Hint: Get the the top 10 otu_ids and map them in descending order  
     //  so the otu_ids with the most bacteria are last. 
-    var sorted_otu_ids = otu_ids.sorted_otu_ids((a, b) =>
-      a.)
 
-    var yticks = ;
+    var yticks = 
 
     // 8. Create the trace for the bar chart. 
     var barData = [

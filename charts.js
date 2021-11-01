29eafef5d670324df545e54828ad8ccbd29177cf
Elle Jacobs <enj657@gmail.com>
2021-11-01 18:55:15 -0500

    modifying charts.js

--- charts.js
+++ charts.js
@@ -58,35 +58,42 @@ function buildCharts(sample) {
   // 2. Use d3.json to load and retrieve the samples.json file 
   d3.json("samples.json").then((data) => {
     // 3. Create a variable that holds the samples array. 
-    var samples_array = data.samples_array;
+    var samples = data.samples;
     // 4. Create a variable that filters the samples for the object with the desired sample number.
-    var samples_filter = samples_array.filter(sampleObj => sampleObj.id == sample);
+    var resultArray = samples.filter(sampleObj => sampleObj.id == sample);
     //  5. Create a variable that holds the first sample in the array.
-    var result = samples_filter[0];
+    var result = resultArray[0];
 
     // 6. Create variables that hold the otu_ids, otu_labels, and sample_values.
-    otu_ids = result.otu_ids
-    otu_labels = result.otu_labels
-    sample_values = result.sample_values
+    var otu_ids = result.otu_ids;
+    var otu_labels = result.otu_labels;
+    var sample_values = result.sample_values;
 
     // 7. Create the yticks for the bar chart.
     // Hint: Get the the top 10 otu_ids and map them in descending order  
     //  so the otu_ids with the most bacteria are last. 
 
-    var yticks = [];
+    var yticks = otu_ids.slice(0,10).reverse();
 
     // 8. Create the trace for the bar chart. 
-    var barData = [
-      
-    ];
+    var barData = [{
+      x: yticks.map(id => sample_values[otu_ids.indexOf(id)]),
+      y: yticks.map(id => "OTU " + id),
+      type: "bar",
+      orientation: 'h',
+      hovertemplate: otu_labels
+    }];
     // 9. Create the layout for the bar chart. 
     var barLayout = {
-      title: "title",
+      title: "Top 10 Bacteria Cultures Found",
       xaxis: {title: "xaxis"},
-      yaxis: {title: "yaxis"},
-      type: "bar"
-    };
+      yaxis: {title: "yaxis"}
+    }
+
+    console.log(yticks.map(id => sample_values[otu_ids.indexOf(id)]))
+    console.log(yticks.map(id => "OTU " + id))
+
     // 10. Use Plotly to plot the data with the layout. 
-    Plotly.newPlot("plot", barData, barLayout);
+    Plotly.newPlot("bar", barData, barLayout);
   });
 }

10488a1821aa85b7f42958b541fffa2cbc362bbb
Elle Jacobs <enj657@gmail.com>
2021-11-01 13:59:45 -0500

    adding data

--- js/charts.js
+++ js/charts.js
@@ -58,14 +58,16 @@ function buildCharts(sample) {
   // 2. Use d3.json to load and retrieve the samples.json file 
   d3.json("samples.json").then((data) => {
     // 3. Create a variable that holds the samples array. 
-
+    var samples_array = data.samples_array;
     // 4. Create a variable that filters the samples for the object with the desired sample number.
-
+    var samples_filter = samples_array.filter(sampleObj => sampleObj.id == sample);
     //  5. Create a variable that holds the first sample in the array.
-
+    var result = samples_filter[0];
 
     // 6. Create variables that hold the otu_ids, otu_labels, and sample_values.
-
+    otu_ids = result.otu_ids
+    otu_labels = result.otu_labels
+    sample_values = result.sample_values
 
     // 7. Create the yticks for the bar chart.
     // Hint: Get the the top 10 otu_ids and map them in descending order  
@@ -79,9 +81,12 @@ function buildCharts(sample) {
     ];
     // 9. Create the layout for the bar chart. 
     var barLayout = {
-     
+      title: "title",
+      xaxis: {title: "xaxis"},
+      yaxis: {title: "yaxis"},
+      type: "bar"
     };
     // 10. Use Plotly to plot the data with the layout. 
-    
+    Plotly.newPlot("plot", barData, barLayout);
   });
 }
